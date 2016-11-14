2b898905c98506a2fd1de83e0f912e112c0bdd03
Edward Medvedev <edward.medvedev@gmail.com>
2016-11-14 12:23:13 +0700

    Prompt fixes, spaces/tabs, stop salt-minion

--- provision.sh
+++ provision.sh
@@ -3,18 +3,19 @@
 ROLE=$1
 
 # Install saltstack
-add-apt-repository ppa:saltstack/salt
+add-apt-repository ppa:saltstack/salt -y
 apt-get update
-apt-get install salt-minion
+apt-get install salt-minion -y
 
 # Configure a standalone minion and a role
 cp salt/config/standalone.conf /etc/salt/minion.d/
 cat <<EOF >> /etc/salt/minion.d/standalone.conf
 grains:
   roles:
-  	- $ROLE
+    - $ROLE
 EOF
 
+systemctl stop salt-minion
 systemctl disable salt-minion
 
 # Apply state
