4d5b0672fb67449af23dd23632d58b629b3a0ac5
Edward Medvedev <edward.medvedev@gmail.com>
2016-11-14 12:57:17 +0700

    Use official repos instead of ppa

--- provision.sh
+++ provision.sh
@@ -3,8 +3,9 @@
 ROLE=$1
 
 # Install saltstack
-add-apt-repository ppa:saltstack/salt -y
-apt-get update
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" >> /etc/apt/sources.list.d/saltstack.list
+apt-get update -y
 apt-get install salt-minion -y
 
 # Configure a standalone minion and a role
