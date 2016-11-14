6cd9c659f648d6f774acb8a953aec2a2bb6ec175
Edward Medvedev <edward.medvedev@gmail.com>
2016-11-14 15:36:27 +0700

    Use saltstack bootstrap

--- provision.sh
+++ provision.sh
@@ -6,10 +6,10 @@ ID=$2
 hostnamectl set-hostname "reynholm-$ROLE-$ID"
 
 # Install saltstack
-wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" >> /etc/apt/sources.list.d/saltstack.list
-apt-get update -y
-apt-get install salt-minion python-pygit2 -y
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
+sudo sh install_salt.sh -P -X -d
+
+apt-get install python-dev python-pip python-git -y
 
 # Configure a standalone minion and a role
 cp salt/config/minion.conf /etc/salt/minion.d/
@@ -17,9 +17,6 @@ cat <<EOF >> /etc/salt/minion.d/minion.conf
 grains:
   role: $ROLE
 EOF
-
-systemctl stop salt-minion
-systemctl disable salt-minion
 
 # Apply state
 salt-call state.apply
