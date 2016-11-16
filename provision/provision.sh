69f45a0b94d256629c1e5edfb11e0f6b0f8cb7eb
Edward Medvedev <edward.medvedev@gmail.com>
2016-11-16 21:13:43 +0700

    Remove the -cp line, fix curl syntax

--- provision/provision.sh
+++ provision/provision.sh
@@ -3,14 +3,13 @@
 ROLE=$1
 
 # Install saltstack
-curl -sL https://bootstrap.saltstack.com | bash install_salt.sh -s -- -P -X -d
+curl -sL https://bootstrap.saltstack.com | bash -s -- -P -X -d
 
 # Install git-python
 apt-get install python-git -y
 
 # Configure a standalone saltstack minion
 curl -sL https://raw.githubusercontent.com/emedvedev/reynholm-ops/master/provision/salt-minion.conf -o /etc/salt/minion.d/minion.conf
-cp salt/config/minion.conf /etc/salt/minion.d/
 cat <<EOF >> /etc/salt/minion.d/minion.conf
 
 grains:
